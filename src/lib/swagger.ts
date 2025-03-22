fd8bfeeb8ac697e9b9840d3ff0247eafabce6dd3
Francis Njuguna <francis@senga.co>
2025-03-22 17:52:39 +0300

    bug: fix the apiFolder in the createSwaggerSpec option.

--- src/lib/swagger.ts
+++ src/lib/swagger.ts
@@ -2,7 +2,7 @@ import { createSwaggerSpec } from "next-swagger-doc";
 
 export const getApiDocs = async () => {
   const spec = createSwaggerSpec({
-    apiFolder: "app/api/v1",
+    apiFolder: "src/app/api/v1",
     definition: {
       openapi: "3.0.0",
       info: {
