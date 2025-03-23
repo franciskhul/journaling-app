0b5d299bb2a2a1215dc1093b49cd1b881e8473d6
Francis Njuguna <francis@senga.co>
2025-03-23 14:31:21 +0300

    refactor: refactor the verifyToken to make it more typescript safe.

--- src/lib/jwt.ts
+++ src/lib/jwt.ts
@@ -25,12 +25,12 @@ export function generateRefreshToken(user: TokenPayload) {
   return jwt.sign({ id: user.id }, REFRESH_TOKEN_SECRET, { expiresIn: "30d" });
 }
 
-export function verifyToken(
+export function verifyToken<T extends JwtPayload = JwtPayload>(
   token: string,
   secret: Secret
-): JwtPayload | string | null {
+): T | null {
   try {
-    return jwt.verify(token, secret);
+    return jwt.verify(token, secret) as T;
   } catch (error) {
     if (error instanceof jwt.JsonWebTokenError) {
       console.error("JWT Error:", error.message);
@@ -38,8 +38,10 @@ export function verifyToken(
       console.error("Token Expired:", error.message);
     } else if (error instanceof jwt.NotBeforeError) {
       console.error("Token Not Active Yet:", error.message);
+    } else if (error instanceof Error) {
+      console.error("Unknown JWT Error:", error.message);
     } else {
-      console.error("Unknown Error:", (error as Error).message);
+      console.error("Unexpected Error:", error);
     }
     return null;
   }
