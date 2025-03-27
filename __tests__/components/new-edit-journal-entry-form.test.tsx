9eef076c75afc23e1b3733a49e169583ea69d7c8
Francis Njuguna <francis@senga.co>
2025-03-27 13:52:23 +0300

    test: create test 'shows validation errors when submitting empty form' to new-edit-entry-form.test test: create test 'validates title length less than 201' to new-edit-entry-form.test test: create test 'validates content length' to new-edit-entry-form.test

--- __tests__/components/new-edit-journal-entry-form.test.tsx
+++ __tests__/components/new-edit-journal-entry-form.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import NewEditJournalEntryForm from "@/components/my-journal/new-edit-journal-entry-form";
-// import { Plus, Edit } from "lucide-react";
 
 const mockJournalEntry = {
   title: "Existing Entry",
@@ -38,6 +37,60 @@ describe("NewEditJournalEntryForm", () => {
         screen.getByDisplayValue(mockJournalEntry.content)
       ).toBeInTheDocument();
       expect(screen.getByText("Save Changes")).toBeInTheDocument();
+    });
+  });
+
+  describe("Form Validation", () => {
+    it("shows validation errors when submitting empty form", async () => {
+      render(<NewEditJournalEntryForm editing={false} />);
+
+      fireEvent.click(screen.getByRole("button", { name: /create entry/i }));
+
+      await waitFor(() => {
+        expect(screen.getByText("Title is required")).toBeInTheDocument();
+        expect(
+          screen.getByText("Please select a category")
+        ).toBeInTheDocument();
+        expect(
+          screen.getByText("Content must be at least 4 characters")
+        ).toBeInTheDocument();
+      });
+    });
+
+    it("validates title length less than 201", async () => {
+      render(<NewEditJournalEntryForm editing={false} />);
+
+      const titleInput = screen.getByLabelText("Title");
+      await fireEvent.change(titleInput, {
+        target: { value: "a".repeat(201) },
+      });
+      await fireEvent.blur(titleInput);
+      await fireEvent.click(
+        screen.getByRole("button", { name: /create entry/i })
+      );
+
+      await waitFor(() => {
+        expect(
+          screen.getByText("Title must be less than 200 characters")
+        ).toBeInTheDocument();
+      });
+    });
+
+    it("validates content length", async () => {
+      render(<NewEditJournalEntryForm editing={false} />);
+
+      const contentInput = screen.getByLabelText("Your Thoughts");
+      await fireEvent.change(contentInput, {
+        target: { value: "a".repeat(3) },
+      });
+      await fireEvent.blur(contentInput);
+      await fireEvent.click(
+        screen.getByRole("button", { name: /create entry/i })
+      );
+
+      expect(
+        await screen.findByText("Content must be at least 4 characters")
+      ).toBeInTheDocument();
     });
   });
 });
