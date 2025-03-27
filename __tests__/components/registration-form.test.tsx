c4b6de9694dd1d318e98a1317e610984f00c38c0
Francis Njuguna <francis@senga.co>
2025-03-27 06:37:35 +0300

    test: add test 'toggles password visibility' to the registration-form test.

--- __tests__/components/registration-form.test.tsx
+++ __tests__/components/registration-form.test.tsx
@@ -1,3 +1,4 @@
+// __tests__/components/RegistrationForm.test.tsx
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import RegistrationForm from "@/components/registration/registration-form";
 // import RegistrationForm from "@/components/RegistrationForm";
@@ -28,5 +29,38 @@ describe("RegistrationForm", () => {
     expect(
       screen.getByRole("button", { name: /Start Journaling/ })
     ).toBeInTheDocument();
+  });
+
+  it("toggles password visibility", async () => {
+    render(<RegistrationForm />);
+    const passwordInput = screen.getByLabelText("Password");
+    const confirmInput = screen.getByLabelText("Confirm Password");
+    const toggleButtons = screen.getAllByRole("button", {
+      name: /show password/i,
+    });
+
+    // Password should be hidden by default
+    expect(passwordInput).toHaveAttribute("type", "password");
+    expect(confirmInput).toHaveAttribute("type", "password");
+
+    // Toggle password visibility
+    await fireEvent.click(toggleButtons[0]);
+    expect(passwordInput).toHaveAttribute("type", "text");
+    expect(confirmInput).toHaveAttribute("type", "password");
+
+    // Toggle confirm password visibility
+    await fireEvent.click(toggleButtons[1]);
+    expect(passwordInput).toHaveAttribute("type", "text");
+    expect(confirmInput).toHaveAttribute("type", "text");
+
+    // click again to hide
+    await fireEvent.click(toggleButtons[0]);
+    expect(passwordInput).toHaveAttribute("type", "password");
+    expect(confirmInput).toHaveAttribute("type", "text");
+
+    // Toggle confirm password visibility
+    await fireEvent.click(toggleButtons[1]);
+    expect(passwordInput).toHaveAttribute("type", "password");
+    expect(confirmInput).toHaveAttribute("type", "password");
   });
 });
