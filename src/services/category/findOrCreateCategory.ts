af723a695967df32dcb731fcb18d0c58cbc98a1a
Francis Njuguna <francis@senga.co>
2025-03-28 15:18:53 +0300

    refactor: refactor the findOrCreateCategory to accept tx argument which can be a TransactionClient | PrismaClient and add error handling.

--- src/services/category/findOrCreateCategory.ts
+++ src/services/category/findOrCreateCategory.ts
@@ -1,22 +1,30 @@
 import { db } from "@/lib/db";
+import type { TransactionClient } from "@/lib/types/prisma";
+import { PrismaClient } from "@prisma/client";
 
 export default async function findOrCreateCategory(
   userId: string,
-  categoryInput: string
+  categoryInput: string,
+  tx: TransactionClient | PrismaClient = db
 ) {
-  const existingCategory = await db.category.findUnique({
-    where: { id: categoryInput },
-  });
+  try {
+    const existingCategory = await tx.category.findUnique({
+      where: { id: categoryInput },
+    });
 
-  if (existingCategory) return existingCategory;
+    if (existingCategory) return existingCategory;
 
-  return await db.category.create({
-    data: {
-      name: categoryInput,
-      systemGenerated: false,
-      UserCategory: {
-        create: { userId },
+    return await db.category.create({
+      data: {
+        name: categoryInput,
+        systemGenerated: false,
+        UserCategory: {
+          create: { userId },
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    console.error("[CATEGORY_SERVICE_ERROR]", error);
+    throw error;
+  }
 }
