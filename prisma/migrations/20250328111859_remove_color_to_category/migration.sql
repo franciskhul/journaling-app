4580e694ae505a617e0b0969c402e65a8029333b
Francis Njuguna <francis@senga.co>
2025-03-28 14:19:49 +0300

    refactor: remove the color column on the Category table.

---
+++ prisma/migrations/20250328111859_remove_color_to_category/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `color` on the `Category` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Category" DROP COLUMN "color";
